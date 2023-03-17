9d44d7aaa3facbb1c00e06c3ef8f217393d53839
hrodriguezgi <hrodriguezgi@gmail.com>
2023-03-16 19:03:16 -0500

    Added Files for boto3 class

--- clase_12/aws.py
+++ clase_12/aws.py
@@ -1,6 +1,7 @@
 import os
+import json
 
-from custom_boto3 import MyS3
+from custom_boto3 import MyS3, MyEc2, MyRds
 
 # first at all: we need AWS credentials to work with boto3
 aws_access_key_id = os.getenv('aws_access_key_id')
@@ -12,15 +13,25 @@ def main():
     s3_obj = MyS3(aws_access_key_id, aws_secret_access_key, aws_region)
 
     #print('listando objetos de un bucket con client')
-    list_bucket = s3_obj.client_list_objects('kubicall')
+    list_bucket = s3_obj.client_list_objects('kubicall', 2)
     for file in list_bucket:
         print(file.get('Key'))
     print()
 
-    """print('listando objetos de un bucket con resource')
-    list_bucket = s3_obj.client_list_objects('kubicall')
-    print(list_bucket)"""
+    bucket_policy = s3_obj.resource_bucket_policy('kubicall')
+    print(bucket_policy.policy)
 
+    print()
+
+    ec2_obj = MyEc2(aws_access_key_id, aws_secret_access_key, aws_region)
+    equipos = ec2_obj.client_describe_hosts('instance-state-name', 'running')
+    print(equipos.get('Reservations')[0].get('Instances')[0].get('InstanceId'))
+
+    print()
+
+    rds_obj = MyRds(aws_access_key_id, aws_secret_access_key, aws_region)
+    rds = rds_obj.client_describe_db_instances('db-instance-id', 'banca-production')
+    print(rds)
 
 if __name__ == '__main__':
     main()
--- clase_12/custom_boto3.py
+++ clase_12/custom_boto3.py
@@ -31,36 +31,75 @@ class MyS3:
                                        region_name=self.aws_region
                                       )
 
-    def client_list_objects(self, bucket, prefix=''):
+    def client_list_objects(self, bucket, max_keys, prefix=''):
         """
         Reference: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/list_objects_v2.html#
         """
         try:
-            objects = self.client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+            objects = self.client.list_objects_v2(Bucket=bucket, Prefix=prefix, MaxKeys=max_keys)
             if 'Contents' not in objects:
                 return []
             list_objects = objects['Contents']
             while objects['IsTruncated']:
                 objects = self.client.list_objects_v2(
-                    Bucket=bucket, ContinuationToken=objects['NextContinuationToken'])
+                    Bucket=bucket, ContinuationToken=objects['NextContinuationToken'], MaxKeys=max_keys)
                 list_objects.extend(objects['Contents'])
             return [item for item in list_objects if item['Key'].startswith(prefix)]
         except self.client.exceptions.NoSuchBucket:
             raise Exception('Bucket name doesnt exist')
         
-    def resource_list_objects(self, bucket, prefix=''):
-        """
-        Reference: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/list_objects_v2.html#
-        """
-        try:
-            objects = self.client.list_objects_v2(Bucket=bucket, Prefix=prefix)
-            if 'Contents' not in objects:
-                return []
-            list_objects = objects['Contents']
-            while objects['IsTruncated']:
-                objects = self.client.list_objects_v2(
-                    Bucket=bucket, ContinuationToken=objects['NextContinuationToken'])
-                list_objects.extend(objects['Contents'])
-            return [item for item in list_objects if item['Key'].startswith(prefix)]
-        except Exception as e:
-            raise Exception(str(e))
+    def resource_bucket_policy(self, bucket):
+        return self.resource.BucketPolicy(bucket)
+    
+
+class MyEc2:
+    def __init__(self, aws_id, aws_key, aws_region):
+        self.aws_id = aws_id
+        self.aws_key = aws_key
+        self.aws_region = aws_region
+
+        self.client = boto3.client('ec2',
+                                   aws_access_key_id=self.aws_id,
+                                   aws_secret_access_key=self.aws_key,
+                                   region_name=self.aws_region
+                                  )
+        
+        self.resource = boto3.resource('ec2',
+                                       aws_access_key_id=self.aws_id,
+                                       aws_secret_access_key=self.aws_key,
+                                       region_name=self.aws_region
+                                      )
+
+
+    def client_describe_hosts(self, filter_name:str, filter_value: str):
+        response = self.client.describe_instances(
+            Filters=[
+                {'Name': filter_name,
+                 'Values': [filter_value,]
+                },
+            ],
+        )
+        return response
+    
+class MyRds:
+    def __init__(self, aws_id, aws_key, aws_region):
+        self.aws_id = aws_id
+        self.aws_key = aws_key
+        self.aws_region = aws_region
+
+        self.client = boto3.client('rds',
+                                   aws_access_key_id=self.aws_id,
+                                   aws_secret_access_key=self.aws_key,
+                                   region_name=self.aws_region
+                                  )
+
+
+    def client_describe_db_instances(self, filter_name:str, filter_value: str):
+        response = self.client.describe_db_instances(
+            Filters=[
+                {'Name': filter_name,
+                 'Values': [filter_value,]
+                },
+            ],
+        )
+        return response
