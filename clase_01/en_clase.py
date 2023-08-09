80fc3d3d29be94ba5bc1541a3e5b2cdf40dc2aca
hrodriguezgi <hrodriguezgi@gmail.com>
2023-08-08 22:06:16 -0500

    After class

---
+++ clase_01/en_clase.py
@@ -0,0 +1,11 @@
+var = 10
+print(f'var:\t{var}\ntype(var):\t{type(var)}\n')
+
+# Ahora voy a crear una por medio de referencia
+var2:str = 'Hola mundo'
+print(f'var2:\t{var2}\ntype(var2):\t{type(var2)}\n')
+
+
+# otra variable
+f = -20
+print(type(f))
