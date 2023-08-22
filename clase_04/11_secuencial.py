9ccb758363b80e663728ed07105a988683cdd6e3
hrodriguezgi <hrodriguezgi@gmail.com>
2023-08-22 17:58:58 -0500

    Class files

--- clase_04/11_secuencial.py
+++ clase_04/11_secuencial.py
@@ -1,13 +1,17 @@
+"""
 # Ejemplo 1
 a = 20
+print(a)
 b = 10
 c = a - b
 print("La resta es igual a:", c)
-
+"""
 
 
 # Ejemplo 2
 a = ['amarillo', 'rojo', 'azul']
-b = ['cafe', 'morado', 'verde']
+print('lista:', a)
 a.extend(b)
+b = ['cafe', 'morado', 'verde']
+
 print("La unión de las dos listas da como resultado", a)
--- clase_04/12_decision.py
+++ clase_04/12_decision.py
@@ -2,26 +2,51 @@
 # if
 n = 4
 if (n % 2 == 0 or n > 5) and (n % 4 == 1 or n < 5):
+    m = n ** 4
+    print(f'el valor de m es {m}')
     print('n es un número par')
+"""
 
+"""
+# Operadores ternarios
+# on_true if expression else on_false
+n = 258
+m = "Cumple" if (n % 2 == 0 or n > 5) else "No cumple"
+print(m)
 
+n = 3
+m = "Cumple" if (n % 2 == 0 or n > 5) else ("cumple b" if n == 4 else "no cumple b")
 
+En if-else:
+n = 3
+if (n % 2 == 0 or n > 5):
+    m = "Cumple"
+else:
+    if n == 4:
+        m = "cumple b"
+    else:
+        m = "no cumple b"
+
+print(m)
+"""
+
+"""
 # if - else
 n = 14
 if n % 2 == 0:
-    print('n es un número par')
+    print("n es un número par")
 else:
-    print('n es un número impar')
+    print("n es un número impar")
 
-# Lo anterior en operador ternario
-# print('n es un número par') if n % 2 == 0 else print('n es un número impar')
+    # Lo anterior en operador ternario
+    # print('n es un número par') if n % 2 == 0 else print('n es un número impar')
 
-print('lo demás')
+print("lo demás")
 """
 
-
+"""
 # if anidado
-a = 15
+a = 9
 b = 10
 c = 15
 if a > b:
@@ -36,25 +61,20 @@ elif b > c:
     print('b es el más grande de todos')
 else:
     print('c es el más grande de todos')
-
-
-
-
-
-
 """
+
+
 # if - elif - else
 a = 5
 b = 10
 if a == b:
-    print('Los números son iguales')
+    print("Los números son iguales")
 elif a > b:
-    print('a es mayor que b')
+    print("a es mayor que b")
 elif b % a == 0:
     b = b**2
-    print('b es divisible por a')
+    print("b es divisible por a")
     print(b)
-    print(a*b, 'otro texto')
+    print(a * b, "otro texto")
 else:
-    print('b es mayor que a')
-"""
+    print("b es mayor que a")
