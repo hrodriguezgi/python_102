2eaa6785db5ad46972a2008ffd0308079a58e683
hrodriguezgi <hrodriguezgi@gmail.com>
2023-08-31 18:13:24 -0500

    Class 07 Exercise

---
+++ clase_06/20_ejercicio.py
@@ -0,0 +1,32 @@
+# Convertir el siguiente objeto en un json e imprimirlo utilizando
+# una identacion de 2 espacios:
+
+carros = [{
+'referencia': 'Rav4',
+'marca': 'Toyota'
+'cilindraje': '2500 cc',
+'modelo': 2020,
+'precio': 32000,
+'nuevo': False
+},
+{
+'referencia': 'Niro',
+'marca': 'KIA',
+'cilindraje': '1600 cc',
+'precio': 15000,
+'nuevo': True
+}]
+
+
+
+# A partir del archivo adjunto llamado 20_ejercicio.json responder las siguientes preguntas:
+# Cuál es el nombre de la persona que mayor balance tiene? 
+# Cuántos hombres y mujeres hay en el archivo?
+# El usuario Elsie Lowery se encuentra activo?
+# Cuál es la frecuencia de repetición de las frutas
+# Cuál es el promedio de edad de los usuarios
+
+
+# Almacenar la información del archivo anterior en un archivo nuevo donde solo
+# se almacenen las siguientes llaves, tener en cuenta que debe ser un archivo json estándar:
+# idx, isActive, name, email.
