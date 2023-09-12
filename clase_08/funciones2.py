e8ac1f4bba4f0ba976b346b1014e51b8e313ff2b
hrodriguezgi <hrodriguezgi@gmail.com>
2023-09-12 18:14:38 -0500

    Class 09

--- clase_08/funciones2.py
+++ clase_08/funciones2.py
@@ -59,3 +59,43 @@ r_suma, *r_otras = operaciones(12, 43)
 print(f'El resultado de la suma es {r_suma}')
 print(f'El resultado de la resta es {r_otras[0]}')
 print(f'El resultado de la multiplicacion es {r_otras[1]}')
+print()
+
+"""
+Parámetros variables
+"""
+# Posición variable
+def sumar_v2(valor_1, valor_2, *valores):
+    print(type(valores), valores)
+    suma = 0
+    for valor in valores:
+        suma += valor
+    return valor_1 + valor_2 + suma
+
+print(sumar_v2(1,2))
+print()
+print(sumar_v2(1,2,3,4,5,6))
+print()
+
+def sumar_v3(valor_1, valor_2, *args):
+    suma = sum(args)
+    return valor_1 + valor_2 + suma
+
+print(f'El resultado de la suma es: {sumar_v3(1,2)}')
+print()
+print(f'El resultado de la suma es: {sumar_v3(1,2,3,4,5,6)}')
+print()
+
+
+def sumar_v4(valor_1, valor_2=10, *args):
+    suma = sum(args)
+    return valor_1 + valor_2 + suma
+
+print(f'El resultado de la suma_v4 es: {sumar_v4(2)}')
+print()
+print(f'El resultado de la suma_v4 es: {sumar_v4(3,12)}')
+print()
+print(f'El resultado de la suma_v4 es: {sumar_v4(1,2,3,4,5,6)}')
+print()
+
+# llave-valor variable
