f60a80430cebc3374908a8f6eddd47290629fb50
hrodriguezgi <hrodriguezgi@gmail.com>
2023-09-12 19:07:42 -0500

    Class 09

--- clase_08/funciones2.py
+++ clase_08/funciones2.py
@@ -99,3 +99,73 @@ print(f'El resultado de la suma_v4 es: {sumar_v4(1,2,3,4,5,6)}')
 print()
 
 # llave-valor variable
+import json
+
+def crear_estudiante(nombre, apellido, activo, **datos):
+    estudiante = dict()
+    estudiante['nombre'] = nombre
+    estudiante['apellido'] = apellido
+    estudiante['activo'] = activo
+    #print(type(datos), datos)
+    estudiante.update(datos)
+    return json.dumps(estudiante)
+
+print(crear_estudiante('Andres', 'Toro', False))
+print()
+print(crear_estudiante('Carlos', 'Aguirre', False, edad=10, sexo='Masculino', grado='quinto'))
+print()
+print(crear_estudiante('Eduard', 'Hoyos', False, edad=12, grado='septimo'))
+print()
+
+# Función con *args y **kwargs
+def completa(val_1, val_2, val_3=10, *vals, **kwvals):
+    mul = val_1 * val_2 * val_3
+    suma = sum(vals)
+    dicc = { key: value for key, value in kwvals.items() }
+    return mul, suma, dicc
+
+print(completa(10, 2, 4, 5, 6, nombre='Harvey', apellido='Rodriguez'))
+print(completa(30, 4))
+print()
+
+
+# Recursividad
+# factorial de un número es la multiplicación de todos sus antecesores
+# 5! = 5 x 4 x 3 x 2 x 1
+# 1! = 1
+
+def factorial(x):
+    print(x)
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x - 1)
+    
+#print(factorial(1))
+print(factorial(5))
+# 5 * factorial(4)
+# 5 * 4 * factorial(3)
+# 5 * 4 * 3 * factorial(2)
+# 5 * 4 * 3 * 2 * factorial(1)
+# 5 * 4 * 3 * 2 * 1
+print()
+
+# Funciones de Mayor Jerarquía o Mayor Orden
+def saludo():
+    print('hola')
+
+saludo()
+
+lista = [1,2,3,4,5]
+
+def cuadrado(num):
+    return num ** 2
+
+lista_2 = list(map(cuadrado, lista))
+print(lista_2)
+print()
+
+def operaciones_v2(num_1, num_2, operacion):
+    return operacion(num_1, num_2)
+
+print(operaciones_v2(3, 5, sumar))
